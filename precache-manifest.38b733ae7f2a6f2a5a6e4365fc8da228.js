0e2d49d17c80d7367bd4a0041f9a126d680de17a
Aakash <aakash@Aakashs-MacBook-Air.local>
2019-01-03 14:50:05 +0530

    Updates

---
+++ precache-manifest.38b733ae7f2a6f2a5a6e4365fc8da228.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5acec2cfc6156f01a535",
+    "url": "/schedules-view/static/css/main.ae425fec.chunk.css"
+  },
+  {
+    "revision": "5acec2cfc6156f01a535",
+    "url": "/schedules-view/static/js/main.5acec2cf.chunk.js"
+  },
+  {
+    "revision": "c41def97c43882e4bb2a",
+    "url": "/schedules-view/static/js/1.c41def97.chunk.js"
+  },
+  {
+    "revision": "c5132fd23ca4271eac56",
+    "url": "/schedules-view/static/js/runtime~main.c5132fd2.js"
+  },
+  {
+    "revision": "f5a83c324a52446db370baa0a3bc7777",
+    "url": "/schedules-view/index.html"
+  }
+];
